420f3039cba067eff697d9b896ddfd3e90b87b91
Suketu27 <suketudanke007@gmail.com>
2022-11-01 21:53:20 +0530

    Loan tbale created

--- Suketu/Loan_table.sql
+++ Suketu/Loan_table.sql
@@ -3,7 +3,7 @@ Create table Loan
 Loan_Amount int(20) NOT NULL, 
 Interest_Rate int(10) NOT NULL, 
 Loan_Type varchar(20),
-Remaining_Amount int(20)
+Remaining_Amount int(20),
  CONSTRAINT PK_Loan PRIMARY KEY (Loan_ID));
 
 
